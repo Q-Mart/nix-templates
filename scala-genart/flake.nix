8880f63964fd285f02d2f856510897226032d6e9
Q-Mart <qumarthjash@gmail.com>
2022-11-06 12:55:27 +0000

    Simple syntax errors in flake.nix

--- scala-genart/flake.nix
+++ scala-genart/flake.nix
@@ -1,16 +1,32 @@
 {
-  description - "Build scala source and run a shell with genart packages";
+  description = "Build scala source and run a shell with genart packages";
 
   inputs.utils.url = "github:numtide/flake-utils";
-  inputs.sbt.url = "github:zaninime/sbt-derivation/refactor";
+  inputs.sbt.url = "github:zaninime/sbt-derivation";
   inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {self, nixpkgs, utils, sbt}: utils.lib.eachDefaultSystem(system: {
-    packages.default = sbt.lib.mkSbtDerivation {
-      pname = "my-scala-package";
-      src = self;
-    };
+  outputs = { self, nixpkgs, utils, sbt }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (self: super: { jre = super.jdk11; }) ];
+        };
+      in {
+        packages.default = sbt.lib.mkSbtDerivation {
+          pname = "my-scala-package";
+          src = self;
+        };
 
-    devShell = pkgs.mkShell {buildInputs = with pkgs; [jdk11 scala scalafmt sbt imagemagick ffmpeg ]}
-  });
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            jdk11
+            scala
+            scalafmt
+            sbt
+            imagemagick
+            ffmpeg
+          ];
+        };
+      });
 }
