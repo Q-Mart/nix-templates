82c7ec6d37553a6eed7a4fcece50ed1e58c1ad1a
Q-Mart <qumarthjash@gmail.com>
2022-11-06 13:09:09 +0000

    Direnv working for scala genart

--- scala-genart/flake.nix
+++ scala-genart/flake.nix
@@ -2,10 +2,10 @@
   description = "Build scala source and run a shell with genart packages";
 
   inputs.utils.url = "github:numtide/flake-utils";
-  inputs.sbt.url = "github:zaninime/sbt-derivation";
-  inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.sbtBuild.url = "github:zaninime/sbt-derivation";
+  inputs.sbtBuild.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, utils, sbt }:
+  outputs = { self, nixpkgs, utils, sbtBuild }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -13,7 +13,7 @@
           overlays = [ (self: super: { jre = super.jdk11; }) ];
         };
       in {
-        packages.default = sbt.lib.mkSbtDerivation {
+        packages.default = sbtBuild.lib.mkSbtDerivation {
           pname = "my-scala-package";
           src = self;
         };
