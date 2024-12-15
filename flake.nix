8dc66adb63c4155a18ffa04bc5da24eabb04a098
Qumarth Jash <qumarthjash@gmail.com>
2024-12-15 12:27:23 +0000

    Remove dotfiles and add welcomeText

--- flake.nix
+++ flake.nix
@@ -6,6 +6,15 @@
       genart-java = {
         path = ./genart-java;
         description = "Basic Java project for creating generative art";
+        welcomeText = ''
+          #Basic Java project for creating generative art
+          This downloads cookiecutter and the required JDK.
+          The flake runs cookiecutter to generate a boilerplate project with
+          processing & QLib imported
+
+          ## Get started
+          Run cookiecutter to build out the template
+          '';
       };
     };
   };
