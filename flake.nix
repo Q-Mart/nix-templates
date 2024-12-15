b0752e738aefbca0c9ee3ba19561223a8cd931e7
Qumarth Jash <qumarthjash@gmail.com>
2024-12-15 16:19:08 +0000

    Minor fixes from testing

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
         description = "Basic Java project for creating generative art";
         welcomeText = ''
           #Basic Java project for creating generative art
+
           This downloads cookiecutte, the JDK, and any required external libraries for Processing.
           The flake runs cookiecutter to generate a boilerplate project with
           processing & QLib imported
